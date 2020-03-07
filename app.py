a0ee6534c6f6439552df00820b4710ccb9c9cea6
Caleb Villalba <49006430+KirisuteG@users.noreply.github.com>
2020-03-07 12:02:36 -0500

    barely know what im doing

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # mysql connection
 
-
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "199856"
+app.config['MYSQL_PORT'] = 3306
 app.config["MYSQL_DB"] = "database0"
 
 mysql = MySQL(app)
